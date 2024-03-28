e049b61eecde57ee9a4ea2df75c403979a4656be
Kathiravan A <140684362+Kathiravan2912@users.noreply.github.com>
2024-03-28 13:02:21 +0530

    Delete javascript (self) directory

--- javascript (self)/task2.js
+++
@@ -1,6 +0,0 @@
-var h1=document.getElementById("res")
-function add(){
-    var repeat= document.createElement("h1")
-    repeat.textContent = "Hi_Bharat...India lost the match"
-    h1.append(repeat)
-}
