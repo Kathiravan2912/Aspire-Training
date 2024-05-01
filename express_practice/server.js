0b8a6b85da0f4d9aba90b72149f4a992d58c2f8f
Kathiravan2912 <kathiravan2002anbarasan@gmail.com>
2024-05-02 02:05:30 +0530

    updated codes

--- express_practice/server.js
+++ express_practice/server.js
@@ -1,13 +1,20 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 
 var server = express();
 
-server.post("/",(req,res)=>{
-    res.send("<h1>This is express js..!</h1>")
+server.get("/register",(req,res)=>{
+    // res.send("<h1>This is express js..!</h1>")
+    server.use(bodyParser.urlencoded({extended:false}))
+    var username = req.query['name'];
+    res.send("<h1> Welcome "+ username + " ...!"+"</h1>"); 
+    // var usermail = req.query['email'];
+    // res.send("<h1> Welcome "+ usermail + " ...!"+"</h1>");
 })
 
 server.get("/admin",(req,res)=>{
-    res.send("<h1>This is Admin page..!</h1>")
+    res.send("<h1>This is Admin page..!</h1>");
+   
 })
 
 // server.post("/admin",(req,res)=>{
