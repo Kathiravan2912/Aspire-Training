5fb873050e69fd45f5d97e6457115bc3d06fb886
Kathiravan2912 <kathiravan2002anbarasan@gmail.com>
2024-05-02 01:33:06 +0530

    modified demo.js

---
+++ express_practice/demo.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var server = http.createServer((req,res)=>{
+    if(req.url=="/")
+    res.write("<h1>Welcome to the express js learning...!</h1>")
+    else if(req.url="/main")
+    res.write("<h1>You are on Main page...!</h1>")
+})
+
+server.listen(1000,()=>{
+    console.log("Your server is running successfully..!")
+})
