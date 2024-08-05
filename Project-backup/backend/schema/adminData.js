465e0bd2c35e59886b7ecea147d666732acf10cd
Kathiravan A <140684362+Kathiravan2912@users.noreply.github.com>
2024-08-06 04:46:47 +0530

    Delete Project-backup directory

--- Project-backup/backend/schema/adminData.js
+++
@@ -1,9 +0,0 @@
-const mongoose  = require('mongoose');
-const DataSchema = new mongoose.Schema({
-    Name:String,
-    Email:String,
-    Password:String
-})
-
-const dataModel = mongoose.model("signupdata",DataSchema)
-module.exports = dataModel
